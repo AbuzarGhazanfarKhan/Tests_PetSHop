b1b2fba34ffe1f4602add7f6d1d6db1e85d97cf2
ahmedraza90 <34ahmed.raza@gmail.com>
2023-01-12 20:41:53 +0500

    completed

--- utils/test_cases.py
+++ utils/test_cases.py
@@ -12,13 +12,13 @@ testCases = [
     ['TEST_07_LogIn_TEST#02', 'In Login Page, when user login with a valid user, he should see Home Page'],#..6
     ['TEST_09_LogIn_TEST#03', 'In Login Page, when user login with a in-valid user, he should see Error Message'],#..7
     ['TEST_10_LogIn_TEST#04', 'In Login page, when user click "Register" button, he should see Sign up Page'],#..8
-    ['TEST_11_LogIn_TEST#01', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..9
-    ['TEST_12_LogIn_TEST#02', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..10
-    ['TEST_13_LogIn_TEST#03', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..11
-    ['TEST_14_LogIn_TEST#04', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..12
-    ['TEST_15_LogIn_TEST#05', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..13
-    ['TEST_16_LogIn_TEST#06', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..14
-    ['TEST_17_LogIn_TEST#07', 'In SignUp page, when user Submit Empty Form He/She should see a Validation'],#..15
+    ['TEST_11_SIGNUP_TEST#01', 'In SignUp Page, when user Submit Empty Form He/She should see a Validation" '],#..5
+    ['TEST_12_SIGNUP_TEST#02', 'In SignUp Page, If user submits form with wrong userId there should be a validation'],#..6
+    ['TEST_13_SIGNUP_TEST#03', 'In SignUp Page, if password and repeated password do not match then form should not be submitted'],#..7
+    ['TEST_14_SIGNUP_TEST#04', 'In SignUp page, If user insert wrong email then form should not be submitted'],#..8
+    ['TEST_15_SIGNUP_TEST#05', 'In SignUp page, If user insert Incorrect phoneNumber then form should not be submitted'],#..9
+    ['TEST_16_SIGNUP_TEST#06', 'In SignUp page, If user do not insert userId then form will not be submitted'],#..10
+    ['TEST_17_SIGNUP_TEST#07', 'In SignUp page, If user provides all the required data then form will be submitted'],#..11    
     ['TEST_18_Cart_TEST#01', 'On HomePage, Clicking on Cart Icon should load Up cart Page and Cart Table'],#..16
     ['TEST_19_Cart_TEST#02', 'In Shopping Cart Page, If there is nothing added in the cart, Sub Total should be $0.00'],#..17
     ['TEST_20_Cart_TEST#03', 'In Shopping Cart Page, After adding product to cart should calculate subtotal as per the amount of product'],#..18
@@ -26,7 +26,7 @@ testCases = [
     ['TEST_22_Cart_TEST#05', 'In Shopping Cart Page, Proceed to checkout should navigate to loggin page if not loggedin'],#..20
     ['TEST_23_Cart_TEST#06', 'In Shopping Cart Page, Proceed to checkout should navigate to loggin page if not loggedin and after logging in should be redirected to order confirmation'],#..21
     ['TEST_24_Cart_TEST#07', 'Logged in user should smoothly add product to cart , proceed to check out and recieve order receipt'],#..22
-    ['TEST_19_Cart_TEST#02', 'In Shopping Cart Page, Cart Should be empty for first User'],#..23
-    ['TEST_24_Update_Acoount_Info_TEST#07', 'Logged in user should smoothly add product to update_Acoount_Info , proceed to check out and recieve order receipt'],#..24
-    ['TEST_24_Update_Acoount_Info_TEST#07', 'Logged in user should smoothly add product to cart , proceed to check out and recieve order receipt'],#..25
+    ['TEST_25_Cart_TEST#02', 'In Shopping Cart Page, Cart Should be empty for first User'],#..23
+    ['TEST_26_Update_Acoount_Info_TEST#07', 'Logged in user should smoothly add product to update_Acoount_Info , proceed to check out and recieve order receipt'],#..24
+    ['TEST_27_Update_Acoount_Info_TEST#07', 'Logged in user should smoothly add product to cart , proceed to check out and recieve order receipt'],#..25
 ]
